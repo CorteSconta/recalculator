04dbd51b78e0cdb5c02d3683706fb570ce0d8676
CorteSconta <umberto.randon@gmail.com>
2022-10-27 19:16:55 +0200

    Basic functioning calculator

--- script.js
+++ script.js
@@ -1,97 +1,102 @@
-const operate = (n1, operator, n2) => { // Main logic
-  let tot = ""
+const operate = (num1, num2, operator) => {
+  const num1F = parseFloat(num1)
+  const num2F = parseFloat(num2)
 
-  if (operator === "add") {
-    tot = parseFloat(n1) + parseFloat(n2)
-  } else if (operator === "subtract") {
-    tot = parseFloat(n1) - parseFloat(n2)
-  } else if (operator === "multiply") {
-    tot = parseFloat(n1) * parseFloat(n2)
-  } else if (operator === "divide") {
-    tot = parseFloat(n1) / parseFloat(n2)
-  }    
-  return tot  
+  switch(operator) {
+    case "add":
+      return  num1F + num2F
+    case "subtract":
+      return  num1F - num2F
+    case "multiply":
+      return  num1F * num2F
+    case "divide":
+      return  num1F / num2F
+  }
 }
 
-const calculator = document.querySelector('.calculon'); 
-const keys = calculator.querySelector(".calculon_btns"); 
-const display = calculator.querySelector(".calculon_display");
+const calculon = document.querySelector(".calculon");
+const btns = calculon.querySelector(".calculon_btns");
+const display = calculon.querySelector(".calculon_display")
 
-keys.addEventListener('click', e => {
-  if (e.target.matches("button")) {
-    const key = e.target;
-    const action = key.dataset.action 
-    const keyContent = key.textContent 
-    const displayedNum = display.textContent 
-    const previouskeyType = calculator.dataset.previouskeyType
+var storedOne = "";
+var equalTwo;
+let isOperator = false;
 
-     Array.from(key.parentNode.children)
-      .forEach(b => b.classList.remove("is-depressed"))
+btns.addEventListener("click", e => {
+  if(e.target.matches("button")) {
+    const btn = e.target;
+    const btnValue = btn.textContent
+    const operatorBtn = e.target.classList.contains("operator");
+    const currentNum = display.textContent;
+    const opr = btn.dataset.opr
+    const prevBtn = calculon.dataset.prevBtn
 
-    if(!action) {
-      if(displayedNum === "0" || previouskeyType === "operator") {
-        display.textContent = keyContent
-      } else {
-        display.textContent = displayedNum + keyContent
-      }
-      calculator.dataset.previouskeyType = "number"
+    
+
+    if(!operatorBtn) {
+      if(currentNum == "0" || isOperator == true) {
+      display.textContent = btnValue;
+      } else display.textContent = currentNum + btnValue
+
+      isOperator = false;
+      calculon.dataset.prevBtn = "number"
+      console.log("number", storedOne)
     }
 
-    //DECIMAL
-    if (action === "decimal") {
-      if(!display.textContent.includes(".")) {
-        display.textContent = displayedNum + "."
-      } else if (previouskeyType === "operator") {
-        display.textContent = "0."
+    if(operatorBtn) {
+      const firstNum = storedOne;
+      const secondNum = currentNum;
+      const operator = calculon.dataset.opr
+      // prevOperator = e.target.textContent
+
+      if(firstNum && operator && prevBtn !== "operator" && prevBtn !== "equal") {
+        const tempValue = operate(firstNum, secondNum, operator)
+        display.textContent = tempValue
+        storedOne = tempValue
+      } else {
+        storedOne = currentNum;
+
       }
+
+      calculon.dataset.opr = opr
+      calculon.dataset.prevBtn = "operator"
+      isOperator = true;
+      console.log(isOperator, storedOne)
+    }
+
+    if(e.target.classList.contains("decimal")) {
+      if(!currentNum.includes(".")) {
+      display.textContent = currentNum + "."
+      }
+    }
+
+    if(e.target.classList.contains("clear")) {
+      console.log("AC")
+    }
+
+    if(e.target.classList.contains("equal")) {
+      let firstNum = storedOne;
+      let secondNum = currentNum;
+      const operator = calculon.dataset.opr
+
+      if(firstNum) {
+        if(prevBtn == "equal") {
+          firstNum = currentNum
+          secondNum = equalTwo
+        }
+        display.textContent = operate(firstNum, secondNum, operator)
+      }
+
       
-      calculator.dataset.previouskeyType = "decimal"
-    }
 
-    if (
-      action === "add" ||
-      action === "subtract" ||
-      action === "divide" ||
-      action === "multiply"
-    ) {
-      const firstValue = calculator.dataset.firstValue
-      const operator = calculator.dataset.operator
-      const secondValue = displayedNum
-
-      if( // Store the value in dataset
-        firstValue && 
-        operator &&
-        previouskeyType !== "operator"
-      ) {
-        const calcValue = operate(firstValue, operator, secondValue)
-        display.textContent = calcValue
-
-        calculator.dataset.firstValue = calcValue
-      } else {
-        calculator.dataset.firstValue = displayedNum
-      }
-
-      key.classList.add("is-depressed")
-        calculator.dataset.previouskeyType = "operator"
-        calculator.dataset.operator = action // store action into dataset    
-    }
-
-    if (action === "clear") { 
-      calculator.dataset.previouskeyType = "clear"
-    }
-
-    if (action === "operate") {
-      const firstValue = calculator.dataset.firstValue
-      const operator = calculator.dataset.operator
-      const secondValue = displayedNum
-
-      display.textContent = operate(firstValue, operator, secondValue)
-      calculator.dataset.previouskeyType = "operate"
+      calculon.dataset.prevBtn = "equal"
+      isOperator = true;
+      equalTwo = secondNum
+      storedOne = currentNum;
+      console.log("operate", storedOne, equalTwo)
     }
   }
 })
-
-
 
 
 
